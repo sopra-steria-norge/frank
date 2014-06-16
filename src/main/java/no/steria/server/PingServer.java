beda5810947a4bbe49ab8b1d4e1cf24f71a4d6d0
Anders Karlsen <karlsenanders@gmail.com>
2014-06-16 11:31:53 +0200

    Can now deploy

--- src/main/java/no/steria/server/PingServer.java
+++ src/main/java/no/steria/server/PingServer.java
@@ -1,19 +1,46 @@
 package no.steria.server;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class PingServer {
-	public static void main(String[] args) throws Exception {
-		int port = 8081;
-		String paraPort = System.getProperty("pingServer.port");
-		if (paraPort != null && !paraPort.isEmpty()) {
-			port = Integer.parseInt(paraPort);
-		}
-		Server server = new Server(port);
-		server.setHandler(new WebAppContext("src/main/webapp", "/"));
-		server.start();
-		
-		System.out.println("Server started port " + port);
-	}
+
+    private final Integer port;
+    private String warFile;
+
+    public PingServer(Integer port, String warFile) {
+        this.port = port;
+        this.warFile = warFile;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String warFile = null;
+        if (args.length > 0) {
+            warFile = args[0];
+        }
+        new PingServer(getPort(8081),warFile).start();
+    }
+
+    private void start() throws Exception {
+        Server server = new Server(port);
+        if (warFile != null) {
+            WebAppContext webAppContext = new WebAppContext();
+            webAppContext.setContextPath("/");
+            webAppContext.setWar(warFile);
+            server.setHandler(webAppContext);
+        } else {
+            HandlerList handlerList = new HandlerList();
+            //handlerList.addHandler(new ShutdownHandler("yablayabla", false, true));
+            handlerList.addHandler(new WebAppContext("src/main/webapp", "/"));
+            server.setHandler(handlerList);
+        }
+        server.start();
+        System.out.println("Running");
+    }
+
+    private static int getPort(int defaultPort) {
+        return defaultPort;
+    }
 }
